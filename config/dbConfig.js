672471c6ac8728ecf6a3d662cf26a19e1384386f
Aspira System <aspiracenter@gmail.com>
2024-02-12 16:13:05 +0530

    sql db changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "crsdatabase",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 module.exports = connection;
